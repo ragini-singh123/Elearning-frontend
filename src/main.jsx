f984190935a7d2332df88cbe869219f9611ece5d
ragini-singh123 <rs5800@srmist.edu.in>
2025-06-24 17:28:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import { UserContextProvider} from "./context/UserContext.jsx";
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server = "http://localhost:8000";
+export const server = "https://elearning-server-3-88ed.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
